0fbb3918e23708dcb14376fb296b001ad682cc99
nereazj544 <nzjvirtual@gmail.com>
2025-05-16 15:47:20 +0200

    feat: add panel proveedor

--- src/app/components/paneles/proveedor/panel-proveedor/panel-proveedor.component.ts
+++ src/app/components/paneles/proveedor/panel-proveedor/panel-proveedor.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-panel-proveedor',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './panel-proveedor.component.html',
   styleUrl: './panel-proveedor.component.css'
 })
