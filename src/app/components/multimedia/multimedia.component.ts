3224f6c9b5b348636cb4af0105dae4a4b33750ad
nereazj544 <nzjvirtual@gmail.com>
2025-05-18 23:27:00 +0200

    feat: agregar componente multimedia con navegación y diseño inicial

--- src/app/components/multimedia/multimedia.component.ts
+++ src/app/components/multimedia/multimedia.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './multimedia.component.css'
 })
 export class MultimediaComponent {
+tabs = ['Libros', 'Peliculas', 'Series', 'Videojuegos'];
+  activeTab = 'Series';
 
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
 }
