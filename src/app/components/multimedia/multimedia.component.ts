33cb68fed913d3f14139085437ba49684da8ff9d
nereazj544 <nzjvirtual@gmail.com>
2025-05-18 23:53:18 +0200

    feat: corregir formato del título en el componente multimedia y agregar módulo común

--- src/app/components/multimedia/multimedia.component.ts
+++ src/app/components/multimedia/multimedia.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-multimedia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './multimedia.component.html',
   styleUrl: './multimedia.component.css'
 })
