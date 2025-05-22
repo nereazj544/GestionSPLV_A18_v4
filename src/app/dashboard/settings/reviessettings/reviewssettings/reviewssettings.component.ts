ddf44c9914dc311819c7e1d55a9f707ad96d3034
nereazj544 <nzjvirtual@gmail.com>
2025-05-22 15:22:49 +0200

    feat: implementar carga de perfil de usuario en el componente Reviewssettings

--- src/app/dashboard/settings/reviessettings/reviewssettings/reviewssettings.component.ts
+++ src/app/dashboard/settings/reviessettings/reviewssettings/reviewssettings.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SupabaseService } from '../../../../shared/service/supabase/data/supabase.service';
+import { Route, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ShowService } from '../../../../shared/service/supabase/show.service';
+
 
 @Component({
   selector: 'app-reviewssettings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './reviewssettings.component.html',
   styleUrl: './reviewssettings.component.css'
 })
-export class ReviewssettingsComponent {
+export class ReviewssettingsComponent implements OnInit {
+  id = '';
+  username = '';
+  imagen_perfil = '';
+constructor(
+  private supabaseService: SupabaseService,
+  private showService: ShowService,
+){}
+
+
+async ngOnInit() {
+    await this.loadUserProfile(); // Cargar el perfil del usuario
+
+  }
+
+  //Cargar el perfil del usuario, para mostrar su imagen y nombre
+  async loadUserProfile() {
+    const userId = await this.supabaseService.getCurrentUserId();
+    if (!userId) {
+      console.error('No hay usuario autenticado');
+      return;
+    }
+    this.id = userId;
+    const { data, error } = await this.showService.getUserProfileById(this.id);
+    if (error) {
+      console.error('Error al obtener perfil:', error);
+      return;
+    }
+    this.username = data.username;
+    this.imagen_perfil = data.imagen_perfil;
+  }
+
+
+
 
 }
