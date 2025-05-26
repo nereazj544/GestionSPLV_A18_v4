9a1f86759f5aef8e160648ade32bb039b031d55f
nereazj544 <nzjvirtual@gmail.com>
2025-05-26 09:14:37 +0200

    feat: agregar respaldo de configuración de Supabase en los entornos

--- src/app/env/env.ts
+++ src/app/env/env.ts
@@ -13,4 +13,12 @@ export const environment = {
     production: true,
     supabaseUrl: 'https://tstpedxohgaopfyialvw.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzdHBlZHhvaGdhb3BmeWlhbHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyNjU1NzUsImV4cCI6MjA2MTg0MTU3NX0.Oy0gR3ILhkGp-kY_jWJDbeblnFt9DB8z_xQN3wwMBbs'
+
+    // TODO: Respaldo
+    /*
+    supabaseUrl: 'https://tstpedxohgaopfyialvw.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxdnFrbWRyaGR5dWpxaWF5ZW91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTAxMTYsImV4cCI6MjA2MzIyNjExNn0.M5fLTr_g_qp2-KzXvzzJwOGAjavnlNz-R3tD7ssYG-0'
+     */
+
+
 };
